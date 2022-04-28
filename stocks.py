452bec56528ed5423ff01f34abf1491d8877fa1e
Neil Martin <87085416+neilmartindev@users.noreply.github.com>
2022-04-28 16:58:02 +0100

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -1,21 +1,19 @@
-import streamlit as st
 import pandas as pd
 import yfinance as yf
 from yfinance import ticker
+import streamlit as st #fix
 
 st.write(""""
          
 ## Tesla Stock Price
-
 Below is shown the stock price and volume change of Tesla from the start of 2020 to 11/11/2021
-
 """)
 
 tickerSymbol = 'TSLA'
 
 tickerData = yf.Ticker(tickerSymbol)
 
-tickerDf = tickerData.history(period='1d', start='2020-1-01', end='2021-11-11')
+tickerDf = tickerData.history(period='1d', start='2020-1-01', end='2022-04-28')
 
 st.line_chart(tickerDf.Close)
 st.line_chart(tickerDf.Volume)
